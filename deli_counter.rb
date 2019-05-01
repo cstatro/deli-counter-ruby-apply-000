2527af2f4e1ae0866286c7c1d5550fabb9ed0994
Colin Tatro <cstatro@gmail.com>
2019-05-01 04:20:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 # Write your code here.
 
 def line deli_line
-  message = "There are people in Line"
+  message = "The line is currently:"
+  if deli_line.length == 0
+    "There is nobody in line"
+  else
+    deli_line.each_with_index do |patron,i|
+      message += " #{i+1}. #{patron}"
+    end
   message
+  end
 end
