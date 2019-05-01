937e975791b2eb005c003bed92efc885d76b9bb9
Colin Tatro <cstatro@gmail.com>
2019-05-01 04:34:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,4 @@
 # Write your code here.
-
 def line deli_line
   message = "The line is currently:"
   if deli_line.length == 0
@@ -9,5 +8,17 @@ def line deli_line
       message += " #{i+1}. #{patron}"
     end
   puts message
+  end
+end
+
+def take_a_number deli_line, patron
+  deli_line.push patron
+end
+
+def now_serving deli_line
+  if deli_line.length == 0
+    puts "ain't nbodoy home"
+  else
+    patron = deli_line.shift
   end
 end
